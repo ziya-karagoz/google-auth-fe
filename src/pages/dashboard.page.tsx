492c8faa7a2cbd1697869176a9011c5eb8d17c03
ziya.karagoz <ziya.karagoz@technohouse.com.tr>
2023-12-20 16:12:41 +0300

    fix: google btn

--- src/pages/dashboard.page.tsx
+++ src/pages/dashboard.page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const DashboardPage = () => {
+    const navigate = useNavigate();
     const currentUser = JSON.parse(localStorage.getItem("user") || "{}");
 
     return (
@@ -28,9 +29,21 @@ const DashboardPage = () => {
                     <h2 className="text-black font-bold text-2xl tracking-wide">{currentUser.name}</h2>
                 </div>
                 <p className="text-emerald-600 font-semibold mt-2.5">Active</p>
+                {/* logout  */}
+                <button
+                    onClick={() => {
+                        localStorage.removeItem("user");
+                        navigate("/");
+                    }}
+                    type="button"
+                    className="text-white w-full mt-12 bg-[#b81812] hover:bg-[#b81812]/90 focus:ring-4 focus:outline-none focus:ring-[#b81812]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between dark:focus:ring-[#b81812]/55 mr-2 mb-2"
+                >
 
+                    <span className="text-center w-full">Logout</span>
+                </button>
 
             </section>
+
         </section>
     );
 };
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -35,32 +35,46 @@ const HomePage = () => {
     }, []);
 
     return (
-        <div className="w-screen h-screen flex justify-center items-center">
-            <div className="px-6 sm:px-0 max-w-sm">
-                <button
-                    onClick={() => {
-                        void handleSignIn();
-                    }}
-                    type="button"
-                    className="text-white w-full  bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
-                >
-                    <svg
-                        className="mr-2 -ml-1 w-4 h-4"
-                        aria-hidden="true"
-                        focusable="false"
-                        data-prefix="fab"
-                        data-icon="google"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 488 512"
+        <div className="w-screen h-screen flex justify-center items-center flex-col">
+            <div>
+                <div className="px-6 sm:px-0 max-w-sm">
+                    <button
+                        onClick={() => {
+                            void handleSignIn();
+                        }}
+                        type="button"
+                        className="text-[#080808] w-full border border-[#f4f5f0]  bg-white hover:bg-[#F4F5F0]/90 focus:ring-4 focus:outline-none focus:ring-[#F4F5F0]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between dark:focus:ring-[#F4F5F0]/55 mr-2 mb-2"
                     >
-                        <path
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M21.8055 10.0415H21V10H12V14H17.6515C16.827 16.3285 14.6115 18 12 18C8.6865 18 6 15.3135 6 12C6 8.6865 8.6865 6 12 6C13.5295 6 14.921 6.577 15.9805 7.5195L18.809 4.691C17.023 3.0265 14.634 2 12 2C6.4775 2 2 6.4775 2 12C2 17.5225 6.4775 22 12 22C17.5225 22 22 17.5225 22 12C22 11.3295 21.931 10.675 21.8055 10.0415Z" fill="#FFC107" />
+                            <path d="M3.15332 7.3455L6.43882 9.755C7.32782 7.554 9.48082 6 12.0003 6C13.5298 6 14.9213 6.577 15.9808 7.5195L18.8093 4.691C17.0233 3.0265 14.6343 2 12.0003 2C8.15932 2 4.82832 4.1685 3.15332 7.3455Z" fill="#FF3D00" />
+                            <path d="M12.0002 22.0001C14.5832 22.0001 16.9302 21.0116 18.7047 19.4041L15.6097 16.7851C14.6057 17.5456 13.3577 18.0001 12.0002 18.0001C9.39916 18.0001 7.19066 16.3416 6.35866 14.0271L3.09766 16.5396C4.75266 19.7781 8.11366 22.0001 12.0002 22.0001Z" fill="#4CAF50" />
+                            <path d="M21.8055 10.0415H21V10H12V14H17.6515C17.2555 15.1185 16.536 16.083 15.608 16.7855L15.6095 16.7845L18.7045 19.4035C18.4855 19.6025 22 17 22 12C22 11.3295 21.931 10.675 21.8055 10.0415Z" fill="#1976D2" />
+                        </svg>
+                        Sign In With Google<div></div>
+                    </button>
+                </div>
+                <div className="px-6 sm:px-0 max-w-sm">
+                    <button
+                        onClick={() => {
+                            void handleSignIn();
+                        }}
+                        type="button"
+                        className="text-white w-full  bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
                             fill="currentColor"
-                            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                        ></path>
-                    </svg>
-                    Sign In With Google<div></div>
-                </button>
+                            className="bi bi-facebook"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                        </svg>
+                        Sign In With Facebook<div></div>
+                    </button>
+                </div>
             </div>
         </div>
     );
